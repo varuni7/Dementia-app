9ff1b625590a642af14e36d44d7b9a06e39027e5
violet7423 <125081950+violet7423@users.noreply.github.com>
2023-04-04 22:24:39 +0530

    Cleaning up a few things

--- lib/src/activities/reminders_todo_activity/reminders.dart
+++ lib/src/activities/reminders_todo_activity/reminders.dart
@@ -11,8 +11,7 @@ class TodoActivity extends StatefulWidget {
   State<TodoActivity> createState() => _HomeState();
 }
 
-//TODO:the add button for todo must only display for caregiver
-//patient must only be able to view todos not add todos n
+ 
 class _HomeState extends State<TodoActivity> {
   final todosList = Todo.todoList();
   List<Todo> _foundToDo = [];
